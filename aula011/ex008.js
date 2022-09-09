f6b5e5a6d2dd3d3fc12939c709ddb275d52e9199
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-09-09 12:13:42 -0300

    ex_velocidade ex_idade

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 80
+
+console.log(`Seu carro esta a ${velocidade}km/h`)
+if (velocidade > 60){ //Condição Simples Apenas If
+    console.log(`Você tomou uma multa por estar a cima da velocidade permitida. MULTADO!`)
+}
