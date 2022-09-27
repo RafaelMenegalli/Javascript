74471d5b4bdc3d036ccefb9c272f18e028ab8876
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-09-27 12:04:49 -0300

    projeto finalizado

--- aula015/scripts.js
+++ aula015/scripts.js
@@ -8,13 +8,44 @@ function verificar(){
         alert('[ERRO] Verifique os dados e tente Novamente!')
     } else{
         var idade = ano - Number(nascimento.value)
-        var sexo1 = document.getElementsByTagName('sexo')
-        var genero = ''
-        if(sexo1[0].checked){
+        var sexo1 = document.getElementById('imasculino')
+        var sexo2 = document.getElementById('ifeminino')
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        genero = ''
+        if(sexo1.checked){
             genero = 'Homem'
-        } else{
+            if(idade >= 0 && idade <= 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca_homem.jpg')
+            } else if(idade <= 21){
+                img.setAttribute('src', 'imagens/jovem_homem.jpg')
+            } else if(idade <= 50){
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto_homem.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'imagens/idoso_homem.jpg')
+            }
+        } else if(sexo2.checked){
             genero = 'Mulher'
-            resultado.innerHTML = `Detectamos ${genero} com ${idade} Anos de Idade!`
+            if(idade >= 0 && idade <= 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca_mulher.jpg')
+            } else if(idade <= 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem_mulher.jpg')
+            } else if(idade <= 50){
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto_mulher.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'imagens/idoso_mulher.jpg')
+            }
         }
+        resultado.innerHTML = `Detectamos <strong>${genero}</strong> com ${idade} Anos de Idade!`
+        resultado.style.textAlign = 'center'
+        resultado.appendChild(img)
+        img.style.padding = '20px'
     }
 }
