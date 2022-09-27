3c7abd9e01f6415d4ec3f3b9f59c32cee368647e
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-09-27 15:18:49 -0300

    testes

---
+++ aula013b/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do{
+    console.log('Rafael Lindo ' + c)
+    c++
+} while(c <= 10)
+
+/*
+var contador = 1
+while(contador <= 10){
+    console.log('Rafael Lindo ' + contador)
+    //contador = contador+ 1
+    contador++
+}
+*/ 
