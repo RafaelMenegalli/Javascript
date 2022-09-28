6f6e27250f8c250ceb9fed64d3e59ad6cda27508
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-09-28 13:25:27 -0300

    finalizada laços de repetição

--- aula013b/ambiente.js
+++ aula013b/ambiente.js
@@ -1,8 +1,14 @@
-var c = 1
+for(var contador = 1; contador <= 10; contador = contador +1){
+    console.log('Rafael Lindo ' + contador)
+}
+
+
+/*var c = 1
 do{
     console.log('Rafael Lindo ' + c)
     c++
 } while(c <= 10)
+*/
 
 /*
 var contador = 1
---
+++ aula014/amebiente.js
@@ -0,0 +1,5 @@
+console.log('Agora o Progrma irá começar')
+for(var c = 1; c <= 10; c++){
+    console.log(`Rafel Lindo ${c}`)
+}
+console.log('Aqui acaba o Programa')
