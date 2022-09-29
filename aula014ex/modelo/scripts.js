d518550d06216704b3694851078813da3c18c1a7
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-09-28 23:49:13 -0300

    exercicio 1 terminado (logo ex 2)

--- aula014ex/modelo/scripts.js
+++ aula014ex/modelo/scripts.js
@@ -4,17 +4,29 @@ function contar(){
     let passo = document.getElementById('ipasso')
 
     if(inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Por Favor Arrume as Informações')
+        let resultado = document.getElementById('iresultado')
+        resultado.innerHTML = 'Dados Invalidos!'
     }else{
         let resultado = document.getElementById('iresultado')
-        resultado.innerHTML = 'Contando:'
+        resultado.innerHTML = 'Contando: <br>'
 
         let i = Number(inicio.value)
         let f = Number(final.value)
         let p = Number (passo.value)
 
-        for(let contador = i; contador <= f; contador += p){
-            resultado.innerHTML += `${contador} `
+        if(p <= 0){
+            alert('[ERRO] Dados Invalidos. considerando passo (1)')
+            p = 1
+        }
+
+        if(i < f){
+            for(let contador = i; contador <= f; contador += p){
+                resultado.innerHTML += `${contador} \u{1F34C}	`
+            }
+        }else{
+            for(let contador = i; contador >= f; contador -= p){
+                resultado.innerHTML += `${contador} \u{1F34C}`
+            } 
         }
     }
 }
