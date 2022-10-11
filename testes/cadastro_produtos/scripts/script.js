c514e9f6809b9902deebb76f4aec3467e5a783cc
Rafael Menegalli <109773013+RafaelMenegalli@users.noreply.github.com>
2022-10-11 15:19:06 -0300

    projeto cadastro alimento

---
+++ testes/cadastro_produtos/scripts/script.js
@@ -0,0 +1,18 @@
+var produto = document.getElementById('iprodutos')
+var tabela = document.getElementById('itab')
+
+function Cadastrar(){
+    if(produto.value.length == 0 ){
+        alert('[ERRO] Insira um produto"')
+    } else{
+        let item = document.createElement('option')
+        item.text = `${produto.value}`
+        tabela.appendChild(item)
+        produto.value = ''
+        produto.focus()
+    }
+}
+
+function Limpar(){
+    tabela.innerHTML = '' 
+}
